edbd98eafdbc23dd50d821c9775ee1576844cd38
vany <79766479+vanybhops@users.noreply.github.com>
2024-03-17 19:39:25 +0100

    added proxy support and moved from fetch to axios

--- Session.ts
+++ Session.ts
@@ -1,53 +1,80 @@
+import axios, { AxiosResponse } from "axios";
+import { HttpsProxyAgent } from "https-proxy-agent";
 type dataType = "json" | "text" | any;
 class Session {
-    cookies: any={};
-    globalHeaders: any={};
-    async get(url: string, method?:any, returnType?: dataType): Promise<Response | any | string> {
-        const response = await fetch(url, {
+    cookies: any = {};
+    globalHeaders: any = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-GB,en;q=0.9",
+        "cache-control": "max-age=0",
+        "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Microsoft Edge\";v=\"122\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+    };
+    httpsAgent:HttpsProxyAgent<string>|null;
+    constructor(proxyUri: string|null) {
+        if (proxyUri) {
+            this.httpsAgent = new HttpsProxyAgent(proxyUri)    
+        }else{
+            this.httpsAgent = null
+        }
+        
+    }
+    async get(url: string, method?: any, returnType?: dataType): Promise<AxiosResponse<any, any>> {
+        const response = await axios.get(url, {
+            httpsAgent: this.httpsAgent,
             headers: {
                 ...method,
                 ...this.globalHeaders,
                 "cookie": this.getCookies()
             }
         })
-        let responseCookies = response.headers.getSetCookie() 
-        this.setCookies( responseCookies )
-        
+        let responseCookies = response.headers["set-cookie"]
+        this.setCookies(responseCookies)
+
         return this.returnData(returnType, response)
     }
 
-    async post(url: string, method: any, returnType: dataType, body:string): Promise<Response | any | string> {
-        const response = await fetch(
-            url, {
-            headers: {
-                ...method,
-                ...this.globalHeaders,
-                "cookie":this.getCookies()
-            },
-            body:body,
-            method: "POST"
-        })
-        let responseCookies = response.headers.getSetCookie() 
-        this.setCookies( responseCookies )
+    async post(url: string, method: any, returnType: dataType, body: string): Promise<AxiosResponse<any, any>> {
+        const response = await axios.post(
+            url,
+            body
+            , {
+                httpsAgent: this.httpsAgent,
+                headers: {
+                    ...method,
+                    ...this.globalHeaders,
+                    "cookie": this.getCookies()
+                },
+            })
+        let responseCookies = response.headers["set-cookie"]
+        this.setCookies(responseCookies)
         return this.returnData(returnType, response)
     }
-    async put(url: string, method: any, returnType: dataType, body:string): Promise<Response | any | string> {
-        const response = await fetch(
-            url, {
+    async put(url: string, method: any, returnType: dataType, body: string): Promise<AxiosResponse<any, any>> {
+        const response = await axios.put(
+            url, body, {
+            httpsAgent: this.httpsAgent,
+
             headers: {
                 ...method,
                 ...this.globalHeaders,
-                "cookie":this.getCookies()
+                "cookie": this.getCookies()
             },
-            body:body,
-            method: "PUT"
         })
-        let responseCookies = response.headers.getSetCookie() 
-        this.setCookies( responseCookies )
+        let responseCookies = response.headers["set-cookie"]
+        this.setCookies(responseCookies)
         return this.returnData(returnType, response)
     }
 
-    setCookies(newCookies: Array<string>) {
+    setCookies(newCookies: Array<string> | undefined) {
+        if (newCookies == undefined) return;
         for (const cookie of newCookies) {
             let [name, value, _] = cookie.split(";")[0].split("=")
             let newCookieObject = { [name]: value }
@@ -62,13 +89,13 @@ class Session {
         return cookieString;
     }
 
-    async returnData(returnType: dataType, response: Response){
+    async returnData(returnType: dataType, response: AxiosResponse<any, any>) {
         switch (returnType) {
             case "json":
-                const jsonData: any = await response.json();
+                const jsonData: any = await response.data();
                 return jsonData
             case "text":
-                const textData: string = await response.text();
+                const textData: string = await response.data();
                 return textData
             default:
                 return response;
